641ea15e9895773bd716784502f08e462385ac7c
Vladimir Caterov <vlad.caterov@mail.utoronto.ca>
2021-11-26 18:09:26 -0500

    fixed routing issues

--- beethoven-final/src/App.js
+++ beethoven-final/src/App.js
@@ -3,21 +3,18 @@ import Home from './views/Home';
 import About from './views/About';
 import Contact from './views/Contact';
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
     <Router>
-      <div>
-        <div>
           <Routes>
             <Route exact path='/' element={<Home/>}/>
             <Route path='/about' element={<About/>}/>
             <Route path='/contact' element={<Contact/>}/>
           </Routes>
-        </div>
-      </div>
+
     </Router>  
   );
 }
