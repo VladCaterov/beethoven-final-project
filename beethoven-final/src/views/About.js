591cccefc87527f356274312f5319e9680725a10
Vladimir Caterov <vlad.caterov@mail.utoronto.ca>
2021-11-25 19:41:40 -0500

    started about page modifications

--- beethoven-final/src/views/About.js
+++ beethoven-final/src/views/About.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import './css/about.css';
 
 const About = () => {
     return (
         <div>
-            <h1>About</h1>
+            <img src="/images/ludwig_van_beethoven.jpg" alt="Ludwig Van Beethoven" className="about-container__img"/>
+            <div className="about-container">
+
+                <h1 className="about-container__h1">About Beethoven Final</h1>
+                <p className="about-container__p">
+                    Description of about page
+                
+                </p>
+            </div>
         </div>
     );
 }
