6bbdad50625a03e9eee6c4462ac5c69bf8cd721a
ekluthra <ekluthra@yahoo.com>
2021-11-28 20:51:18 -0500

    added carousel, audio, and descriptions

--- beethoven-final/src/components/InfoSection.js
+++ beethoven-final/src/components/InfoSection.js
@@ -1,19 +1,21 @@
 import './InfoSection.css';
-import image from "../images/download.jpg";
 
-function InfoSection(){
+function InfoSection(props){
     return(
         <div className="wrapper">
-            <div className="background-div background-div__blur" style={{backgroundImage: `url(${image})`}}>
+            <div className="background-div background-div__blur" style={{backgroundImage: `url(${props.source})`}}>
 
             </div>
                 <div className="content-container">
+                    <div className="content__audio-container">
+                        <audio src="https://upload.wikimedia.org/wikipedia/commons/1/14/Moonlight_Sonata_Movement_4.wav" controls/>
+                    </div>
                     <div className="content__image-container">
-                        <div className="image-container__image" style={{backgroundImage: `url(${image})`}}></div>
+                        <div className="image-container__image" style={{backgroundImage: `url(${props.source})`}}></div>
                     </div>
                     <div className="content__info-container">
                         <p>
-                            This is where information about the art/the music goes.
+                            {props.description}
                         </p>
                     </div>
                 </div>
