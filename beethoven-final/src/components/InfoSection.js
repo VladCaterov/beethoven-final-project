7137f5a7af4b9b0ddbba2d50fdb6c5bedeebde54
ekluthra <ekluthra@yahoo.com>
2021-11-29 18:22:09 -0500

    added audio sources

--- beethoven-final/src/components/InfoSection.js
+++ beethoven-final/src/components/InfoSection.js
@@ -8,7 +8,7 @@ function InfoSection(props){
             </div>
                 <div className="content-container">
                     <div className="content__audio-container">
-                        <audio src="https://upload.wikimedia.org/wikipedia/commons/1/14/Moonlight_Sonata_Movement_4.wav" controls/>
+                        <audio src={props.audio} controls/>
                     </div>
                     <div className="content__image-container">
                         <div className="image-container__image" style={{backgroundImage: `url(${props.source})`}}></div>
